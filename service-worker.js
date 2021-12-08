a7f99c238d1499fd5db4c9ad98f8b3a38b40bd06
shr1ftyy <syeamtechdemon@gmail.com>
2021-12-09 01:34:32 +1030

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/findswap-interface/precache-manifest.bc1480e2a3e8341fb9764f619cf642c8.js"
+  "/findswap-interface/precache-manifest.ec789d2fdff799e6e3505d036e383382.js"
 );
 
 self.addEventListener('message', (event) => {
